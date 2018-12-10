e51811ed83084d9b3d52b9fa314aec8a7ed092c3
izzijones <izzijones@me.com>
2018-12-10 03:05:25 -0500

    ajax call and conditional output

--- my-app/src/LoadingButton.js
+++ my-app/src/LoadingButton.js
@@ -7,7 +7,10 @@ export default class LoadingButton extends React.Component {
         this.handleClick = this.handleClick.bind(this);
   
         this.state = {
-            isLoading: false
+            error : null,
+            isLoading: false,
+            loaded: false,
+            items : []
         };
     }
   
@@ -17,22 +20,49 @@ export default class LoadingButton extends React.Component {
         console.log(this.props.age);
         console.log(this.props.gender);
 
-        // This probably where you would have an `ajax` call
+        //TODO: change this to real endpoint
+        fetch(`http://127.0.0.1:5000/predict?age=${encodeURIComponent(this.props.age)}&gender=${encodeURIComponent(this.props.gender)}`, {
+        method: "GET",
+        }).then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoading: false,
+            items: result.items, loaded: true
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoading: false,
+            error: true,
+          });
+        }
+      )
         
-        setTimeout(() => {
-            // Completed of async action, set loading state back
-            this.setState({ isLoading: false });
-            }, 2000);
+        // setTimeout(() => {
+        //     // Completed of async action, set loading state back
+        //     this.setState({ isLoading: false });
+        //     }, 2000);
         }
   
     render() {
-        const { isLoading } = this.state;
+            const { error, isLoading, loaded, items } = this.state;
         return (
-            <button
+            <div>
+               <button
                 disabled={isLoading}
                 onClick={!isLoading ? this.handleClick : null}>
                 {isLoading ? 'Loading...' : 'Submit'}
-            </button>
+                </button>
+                <h2>
+                    {loaded ? 'Success! ' : "Please enter your information"}
+                </h2>
+            </div>
+
+
         );
     }
 }
